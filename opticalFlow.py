d600d8a312f1c9964fd441522309118121eb3620
onewhl <onewhl8@gmail.com>
2017-01-12 00:36:19 +0300

    Added reading coordinates of object from a file. Added ROI.

--- opticalFlow.py
+++ opticalFlow.py
@@ -1,41 +1,83 @@
-import numpy as np
 import cv2
 
-#for lucas kanade method
+#PRESS S TO START TRACKING
+
+#For lucas kanade method
 lk_params = dict(winSize=(15,15),
                   maxLevel=2,
                   criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
-#for feature detector
+#For feature detector
 feature_params = dict(maxCorners=100,
                        qualityLevel=0.3,
                        minDistance=7,
                        blockSize=7)
 
-path_to_video = "./data/cos.mov"
-cap = cv2.VideoCapture(path_to_video)
-ret, old_frame = cap.read()
-old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
-mask = np.zeros_like(old_frame)
+path_to_video = "./3031210.mp4"
+file_name = "detected.txt"
 
-while(True):
-    ret, frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
-    good_new = p1[st==1]
-    good_old = p0[st==1]
-    # draw the tracks
-    for i,(new,old) in enumerate(zip(good_new,good_old)):
-        a,b = new.ravel()
-        c,d = old.ravel()
-        cv2.line(mask, (a,b), (c,d), (0, 255, 0), 2)
-        cv2.circle(frame,(a,b), 5, (0, 255, 0), -1)
-    img = cv2.add(frame,mask)
-    cv2.imshow('tracker', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27:
-        break
-    old_gray = frame_gray.copy()
+f = open(file_name, 'r')
+coords = f.read().split(' ')
+coords = [coord.rstrip() for coord in coords]
+
+cap = cv2.VideoCapture(path_to_video)
+cv2.namedWindow('Main')
+
+while(1):
+    while(1):
+        _, frame = cap.read()
+        cv2.rectangle(frame, (int(float(coords[0])), int(float(coords[1]))),
+                      (int(float(coords[2])), int(float(coords[3]))),
+                      color=(100, 255, 100), thickness=4)
+
+        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #ROI
+        roi = frameGray[int(float(coords[1])):int(float(coords[3])),
+              int(float(coords[0])):int(float(coords[2]))]
+
+        #Feature detector
+        new_corners = cv2.goodFeaturesToTrack(roi, **feature_params)
+        new_corners[:, 0, 0] = new_corners[:, 0, 0] + int(float(coords[0]))
+        new_corners[:, 0, 1] = new_corners[:, 0, 1] + int(float(coords[1]))
+
+        for corner in new_corners:
+            cv2.circle(frame, (int(corner[0][0]), int(corner[0][1])), 5, (255, 0, 0))
+
+        oldFrameGray = frameGray.copy()
+        old_corners = new_corners.copy()
+
+        cv2.imshow('Main', frame)
+
+        k = 0xFF & cv2.waitKey(5)
+        if k == 27:
+            cv2.destroyAllWindows()
+            cap.release()
+        elif k == ord('s'):
+            break
+
+    while(1):
+        ret, frame = cap.read()
+        frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        #Optical flow
+        new_corners, p0, p1 = cv2.calcOpticalFlowPyrLK(oldFrameGray, frameGray,
+                                                        old_corners, None, **lk_params)
+        new_corners_updated = new_corners.copy()
+        for corner in new_corners_updated:
+            cv2.circle(frame, (int(corner[0][0]), int(corner[0][1])), 5, (0, 255, 0))
+
+        oldFrameGray = frameGray.copy()
+        old_corners = new_corners_updated.copy()
+
+        cv2.imshow('Main', frame)
+
+        k = 0xFF & cv2.waitKey(5)
+        if k == 27:
+            cv2.destroyAllWindows()
+            cap.release()
+        elif k == ord('s'):
+            break
+
 cv2.destroyAllWindows()
 cap.release()

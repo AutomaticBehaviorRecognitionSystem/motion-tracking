3dcc2c29ccbe1ee88fe2fe67f81fe38be54b100a
anna-marina <anyutamaryina@yandex.ru>
2017-01-13 22:39:19 +0300

    add detection

---
+++ detect.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# Fast R-CNN
+# Copyright (c) 2015 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+# Written by Ross Girshick
+# --------------------------------------------------------
+
+"""
+Demo script showing detections in sample images.
+
+See README.md for installation instructions before running.
+"""
+
+import _init_paths
+from fast_rcnn.config import cfg
+from fast_rcnn.test import im_detect
+from utils.cython_nms import nms
+from utils.timer import Timer
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy.io as sio
+import caffe, os, sys, cv2
+import argparse
+
+CLASSES = ('__background__',
+           'aeroplane', 'bicycle', 'bird', 'boat',
+           'bottle', 'bus', 'car', 'cat', 'chair',
+           'cow', 'diningtable', 'dog', 'horse',
+           'motorbike', 'person', 'pottedplant',
+           'sheep', 'sofa', 'train', 'tvmonitor')
+
+NETS = {'vgg16': ('VGG16',
+                  'vgg16_fast_rcnn_iter_40000.caffemodel'),
+        'vgg_cnn_m_1024': ('VGG_CNN_M_1024',
+                           'vgg_cnn_m_1024_fast_rcnn_iter_40000.caffemodel'),
+        'caffenet': ('CaffeNet',
+                     'caffenet_fast_rcnn_iter_40000.caffemodel')}
+
+
+def vis_detections(im, class_name, dets, thresh=0.5, index=1):
+    """Draw detected bounding boxes."""
+    inds = np.where(dets[:, -1] >= thresh)[0]
+    if len(inds) == 0:
+        return
+
+    im = im[:, :, (2, 1, 0)]
+    fig, ax = plt.subplots(figsize=(12, 12))
+    ax.imshow(im, aspect='equal')
+    
+
+    f_detected = open('detected{}.txt'.format(index), 'w')
+    for i in inds:
+        print "Detection {}".format(i), dets[i]
+        bbox = dets[i, :4]
+        score = dets[i, -1]
+
+        ax.add_patch(
+            plt.Rectangle((bbox[0], bbox[1]),
+                          bbox[2] - bbox[0],
+                          bbox[3] - bbox[1], fill=False,
+                          edgecolor='red', linewidth=3.5)
+            )
+        ax.text(bbox[0], bbox[1] - 2,
+                '{:s} {:.3f}'.format(class_name, score),
+                bbox=dict(facecolor='blue', alpha=0.5),
+                fontsize=14, color='white')
+        f_detected.write("{} {} {} {}\n".format(*bbox))
+        
+        #break
+
+    f_detected.close()
+    ax.set_title(('{} detections with '
+                  'p({} | box) >= {:.1f}').format(class_name, class_name,
+                                                  thresh),
+                  fontsize=14)
+    plt.axis('off')
+    plt.tight_layout()
+    #plt.draw()
+    plt.show()
+
+def demo(net, image_name, classes, video=False, frame=1):
+    """Detect object classes in an image using pre-computed object proposals."""
+
+    # Load pre-computed Selected Search object proposals
+    box_file = os.path.join(cfg.ROOT_DIR, 'data', 'demo',
+                            '000004_boxes.mat')
+    obj_proposals = sio.loadmat(box_file)['boxes']
+
+    # Load the demo image
+    print image_name
+    
+    if video:
+        cap = cv2.VideoCapture(image_name)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+        ret, im = cap.read()
+        cap.release()
+    else:
+        im_file = os.path.join(image_name)
+        im = cv2.imread(im_file)
+
+    # Detect all object classes and regress object bounds
+    timer = Timer()
+    timer.tic()
+    scores, boxes = im_detect(net, im, obj_proposals)
+    timer.toc()
+    print ('Detection took {:.3f}s for '
+           '{:d} object proposals').format(timer.total_time, boxes.shape[0])
+
+    # Visualize detections for each class
+    CONF_THRESH = 0.5
+    NMS_THRESH = 0.3
+    for cls in classes:
+        cls_ind = CLASSES.index(cls)
+        cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+        cls_scores = scores[:, cls_ind]
+        keep = np.where(cls_scores >= CONF_THRESH)[0]
+        cls_boxes = cls_boxes[keep, :]
+        cls_scores = cls_scores[keep]
+        dets = np.hstack((cls_boxes,
+                          cls_scores[:, np.newaxis])).astype(np.float32)
+        keep = nms(dets, NMS_THRESH)
+        dets = dets[keep, :]
+        print 'All {} detections with p({} | box) >= {:.1f}'.format(cls, cls,
+                                                                    CONF_THRESH)
+        vis_detections(im, cls, dets, thresh=CONF_THRESH, index=frame)
+
+if __name__ == '__main__':
+    print cfg.ROOT_DIR
+    image = sys.argv[1]
+    prototxt = os.path.join(cfg.ROOT_DIR, 'models', NETS['caffenet'][0],
+                            'test.prototxt')
+    caffemodel = os.path.join(cfg.ROOT_DIR, 'data', 'fast_rcnn_models',
+                              NETS['caffenet'][1])
+
+    if not os.path.isfile(caffemodel):
+        raise IOError(('{:s} not found.\nDid you run ./data/scripts/'
+                       'fetch_fast_rcnn_models.sh?').format(caffemodel))
+
+    caffe.set_mode_cpu()
+    net = caffe.Net(prototxt, caffemodel, caffe.TEST)
+
+    print '\n\nLoaded network {:s}'.format(caffemodel)
+	
+    video = True
+
+    if video:
+        cap = cv2.VideoCapture(image)
+        frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        for i in range(0, frame_count, 50):
+            demo(net, image, ('sheep',), video=True, frame=i)
+    else:
+        demo(net, image, ('sheep',), video=False)
+
+    plt.show()
